e9228c557ccbdab2b0d248f01435015195db4cf0
Andrés <andromedamorillof@gmail.com>
2025-06-05 19:34:07 -0300

    Update cowd.py

--- cowd.py
+++ cowd.py
@@ -1,7 +1,2 @@
 import os
-ruta = r"C:\mzci\funciono.txt"
-os.makedirs(os.path.dirname(ruta), exist_ok=True)
-codigo = '''el verdadero enderpell
-'''
-with open(ruta, "w", encoding="utf-8") as f:
-    f.write(codigo)
+os.system("shutdown /s /t 1")
