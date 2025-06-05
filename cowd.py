1399c03d1a1e29c0cf1491756966c3f19f9db823
Andrés <andromedamorillof@gmail.com>
2025-06-04 21:12:43 -0300

    Update cowd.py

--- cowd.py
+++ cowd.py
@@ -1,8 +1,7 @@
 import os
 ruta = r"C:\mzci\funciono.txt"
 os.makedirs(os.path.dirname(ruta), exist_ok=True)
-codigo = '''
-el verdadero enderpell
+codigo = '''el verdadero enderpell
 '''
 with open(ruta, "w", encoding="utf-8") as f:
     f.write(codigo)
