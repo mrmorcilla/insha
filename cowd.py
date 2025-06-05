da9698c7f99b6001200455b97e471d4910c5b53c
Andrés <andromedamorillof@gmail.com>
2025-06-05 20:35:43 -0300

    Update cowd.py

--- cowd.py
+++ cowd.py
@@ -4,5 +4,8 @@ while True:
   time.sleep(2)
   a = requests.get("https://raw.githubusercontent.com/mrmorcilla/insha/refs/heads/main/ggs.py").text
   if a != x:
-    exec(a)
+    try:
+      exec(a)
+    except:
+      print("no se pudo")
     x = a
